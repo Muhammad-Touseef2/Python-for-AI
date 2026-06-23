eac4f3e1ef5216c1c166266902cb82f3d99eb3fe
Muhammad Touseef <touseefrajput2008@gmail.com>
2026-06-23 10:56:45 -0700

    Add files via upload

---
+++ Day_01.py
@@ -0,0 +1,3 @@
+#Day 01 of python
+
+print("Hello World")
---
+++ Day_02.py
@@ -0,0 +1,32 @@
+#Day_02.py
+
+# name = "Touseef"
+# age = 18
+# isAdult = True
+# cgpa = 3.44
+
+# print(name)
+# print(age)
+# print(isAdult)
+# print(cgpa)
+
+#taking input
+#python inout always return stirng
+
+#age = int(input("Enter your age : "))
+#print("Your age is " , age)
+
+#name = (input("Enter your name :"))
+#print("Your name is " , name)
+
+
+# marks = int(input("Enter your marks : "))
+
+# if marks >= 85 :
+#     print("A")
+# elif marks >=75 :
+#     print("B")
+# else:
+#     print("C")    
+
+
